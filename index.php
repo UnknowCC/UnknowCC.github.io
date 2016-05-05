d6449a578b16f404bb76317b18cc2b2d36d2629d
wei_ZR <shen_mewei@sina.com>
2016-05-05 22:29:44 +0800

    set up

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My github</title>
+</head>
+<body>
+	<h1>Hello world</h1>
+	<p>It's my first GitHub Pages</p>
+</body>
+</html>
